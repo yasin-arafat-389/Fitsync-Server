b9e89b4f929f009edc7c72f28cee3937217bf82e
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2023-11-23 19:22:04 +0600

    Setup the server side environment

---
+++ index.js
@@ -0,0 +1,118 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const app = express();
+const port = process.env.PORT || 5001;
+
+// Parsers
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+// Middlewares
+// Token Verification
+// const verifyToken = (req, res, next) => {
+//   if (!req.headers.authorization) {
+//     return res.status(401).send({ message: "unauthorized access" });
+//   }
+//   const token = req.headers.authorization.split(" ")[1];
+//   jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+//     if (err) {
+//       return res.status(401).send({ message: "unauthorized access" });
+//     }
+//     req.user = decoded;
+//     next();
+//   });
+// };
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gef2z8f.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  const usersCollection = client.db("A12").collection("users");
+  try {
+    // Token generation API
+    app.post("/access-token", async (req, res) => {
+      let user = req.body;
+      const token = jwt.sign(user, process.env.TOKEN_SECRET, {
+        expiresIn: "365d",
+      });
+      res.send({ token });
+    });
+
+    // Creating a payment intent
+    // app.post("/create-payment-intent", async (req, res) => {
+    //   const { price } = req.body;
+    //   const amount = parseInt(price * 100);
+    //   const paymentIntent = await stripe.paymentIntents.create({
+    //     amount: amount,
+    //     currency: "usd",
+    //     payment_method_types: ["card"],
+    //   });
+
+    //   res.send({
+    //     clientSecret: paymentIntent.client_secret,
+    //   });
+    // });
+
+    // Admin verification
+    // const verifyAdmin = async (req, res, next) => {
+    //   const email = req.decoded.email;
+    //   const query = { email: email };
+    //   const user = await usersCollection.findOne(query);
+    //   const isAdmin = user?.role === "admin";
+    //   if (!isAdmin) {
+    //     return res.status(403).send({ message: "forbidden access" });
+    //   }
+    //   next();
+    // };
+
+    // Store user info in the database
+    // app.post("/users", async (req, res) => {
+    //   const user = req.body;
+    //   const query = { email: user.email };
+    //   const isExists = await usersCollection.findOne(query);
+    //   if (isExists) {
+    //     return res.send({ message: "User already exists" });
+    //   }
+    //   const result = await usersCollection.insertOne(user);
+    //   res.send(result);
+    // });
+
+    // Get specific user data filtered by email
+    // app.get("/users", async (req, res) => {
+    //   const email = req.query.email;
+    //   const query = { email: email };
+    //   const result = await usersCollection.findOne(query);
+    //   res.send(result);
+    // });
+
+    //
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is up and running");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
