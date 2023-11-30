b3757d2c46221a1f9ca4330a85c97a18176a4886
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2023-11-30 22:09:07 +0600

    Added a route to send email to the member upon slot rejection

--- index.js
+++ index.js
@@ -492,6 +492,29 @@ async function run() {
 
       res.json({ pricingData: pricingDataWithImages, trainerDetails });
     });
+
+    // API endpoint to reject a member
+    app.post("/reject-member", express.json(), (req, res) => {
+      const { email } = req.body;
+
+      if (!email) {
+        return res.status(400).json({ error: "Missing required parameters" });
+      }
+
+      try {
+        sendMail(
+          email,
+          "Slot Rejected",
+          "We are very sorry to inform you that your slot has been rejected by the trainer"
+        );
+        res
+          .status(200)
+          .json({ success: true, message: "Email sent successfully" });
+      } catch (error) {
+        console.error("Error sending email:", error);
+        res.status(500).json({ error: "Internal server error" });
+      }
+    });
   } finally {
   }
 }
